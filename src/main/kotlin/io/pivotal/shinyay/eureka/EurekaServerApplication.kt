03a98ded6d8c1116d0e30fe51eb5b104709f8db4
shinyay <shinya.com@gmail.com>
2019-04-30 21:56:50 +0900

    add EnableEurekaServer annotation

--- src/main/kotlin/io/pivotal/shinyay/eureka/EurekaServerApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/eureka/EurekaServerApplication.kt
@@ -2,8 +2,10 @@ package io.pivotal.shinyay.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaServerApplication
 
 fun main(args: Array<String>) {
